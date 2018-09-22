95d7420fd7b6584ba0eb526fa019dd42924af853
Usairim Isani <usairim.isani@outlook.com>
2018-09-22 22:52:00 +0500

    Buil failure fix attempt 2

--- src/store/Tickets/index.js
+++ src/store/Tickets/index.js
@@ -1,6 +1,6 @@
 // This file has been changed
 import state from "./state";
-import mutations from "./mutations";
+import mutations from "./mutations.js";
 import actions from "./actions";
 import getters from "./getters";
 export default {
