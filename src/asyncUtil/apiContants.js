24736c17fa3801692fd7e0be64a3d43d45b052e8
Usairim Isani <usairim.isani@outlook.com>
2020-06-21 15:25:21 +0500

    PJ. Updated along with a few Api Constant

--- src/asyncUtil/apiContants.js
+++ src/asyncUtil/apiContants.js
@@ -1,11 +1,11 @@
-// let SERVER = `localhost`;
-// let SERVER_PORT = `3000`;
+let SERVER = `localhost`;
+let SERVER_PORT = `3000`;
 const Constants = {
   api: ``
 };
 // if (window.location.href.includes("surge")) {
-Constants.api = `https://stormy-depths-62680.herokuapp.com/api`;
+// Constants.api = `https://stormy-depths-62680.herokuapp.com/api`;
 // } else {
-//   Constants.api = `http://${SERVER}:${SERVER_PORT}/api`;
+Constants.api = `http://${SERVER}:${SERVER_PORT}/api`;
 // }
 export default Constants;
