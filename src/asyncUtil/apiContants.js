64fe8ab8d8b48ea7ca50ccc2d4c66070f1c169b9
Usairim Isani <usairim.isani@outlook.com>
2020-06-21 15:42:20 +0500

    Deployed To heroku.

--- src/asyncUtil/apiContants.js
+++ src/asyncUtil/apiContants.js
@@ -1,11 +1,13 @@
+/* eslint-disable */
+
 let SERVER = `localhost`;
 let SERVER_PORT = `3000`;
 const Constants = {
-  api: ``
+  api: ``,
 };
 // if (window.location.href.includes("surge")) {
-// Constants.api = `https://stormy-depths-62680.herokuapp.com/api`;
+Constants.api = `https://cryptopins-api.herokuapp.com/api/`;
 // } else {
-Constants.api = `http://${SERVER}:${SERVER_PORT}/api`;
+// Constants.api = `http://${SERVER}:${SERVER_PORT}/api`;
 // }
 export default Constants;
